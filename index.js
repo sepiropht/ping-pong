dcbcbc805b916805c5d42ffc50cfcf7cdfe60b52
sepiropht <williamelimbi@gmail.com>
2017-10-02 01:24:24 +0200

    communication between clients via websocket

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var through = require("through2");
 var color = randomcolor();
 var cuid = require("cuid");
 var myId = cuid();
+var countMouseMove = 0;
 
 window.addEventListener("mousemove", onMouseMove);
 
@@ -29,23 +30,30 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function square(info) {
-  return `<div class="square" style="top: ${info.y}px; left: ${info.x}px; background:${info.color}" ></div>`;
+function Square(info, index) {
+  return `<div class="square" id= ${index} style="top: ${info.y}px; left: ${info.x}px; background:${info.color}" ></div>`;
 }
 
 function update() {
-  return html.update(root, `${state.squares.map(sq => square(sq))}`);
+  console.log(state.squares.map((sq, index) => Square(sq, index)));
+  return html.update(
+    root,
+    `<div>${state.squares.map((sq, index) => Square(sq, index))}</div>`
+  );
 }
 
 function onMouseMove(e) {
-  ws.write(JSON.stringify({ x: e.clientX, y: e.clientY, id: myId }));
+  countMouseMove++;
+  if (countMouseMove < 5) return;
+  countMouseMove = 0;
+  ws.write(
+    JSON.stringify({ x: e.clientX, y: e.clientY, id: myId, color: color })
+  );
 }
 
 function websocketHandler() {
   return through(function(buf, enc, next) {
-    console.log("avant le parsing", buf.toString());
-    var res = JSON.parse(buf.toString());
-    console.log("retour  socket", res);
+    var res = JSON.parse(buf.toString()) ? JSON.parse(buf.toString()) : {};
     if (state.squares.some(sq => sq.id === res.id)) {
       state.squares.map(sq => {
         if (sq.id === res.id) return Object.assign(sq, { x: res.x, y: res.y });
@@ -54,6 +62,7 @@ function websocketHandler() {
     } else {
       state.squares.push(res);
     }
+    console.log(state);
     update();
     next();
   });
--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var wsock = require("websocket-stream");
 var ecstatic = require("ecstatic");
 var server = http.createServer(ecstatic({ root: `${__dirname}/public` }));
 var ws = wsock.createServer({ server: server }, handle);
-
+var streams = [];
+var count = 0;
 function handle(stream, request) {
-  stream.pipe(stream);
+  streams.push(stream);
+  count++;
+  console.log(count);
+  streams.forEach(str => str.pipe(stream));
+  stream.on("data", function(o) {
+    // console.log(JSON.parse(o.toString()));
+    streams.forEach(function(s) {
+      s.write(o);
+    });
+  });
 }
 server.listen(5000);
 console.log("yeah sever is up");
