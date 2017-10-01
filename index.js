2701524b154e5b65d8e39f4ff79be70d7ee9b0c8
sepiropht <williamelimbi@gmail.com>
2017-10-02 00:20:33 +0200

    render list of squares

--- index.js
+++ index.js
@@ -4,25 +4,24 @@ var ws = websocket("ws://" + window.location.host);
 var root = document.body.appendChild(document.createElement("div"));
 var randomcolor = require("randomcolor");
 var through = require("through2");
-const color = randomcolor();
-ws.pipe(
-  through(function(buf, enc, next) {
-    console.log("avant le parsing", buf.toString());
-    const res = JSON.parse(buf.toString());
-    console.log("retour du socket", res);
-    state.mouse.x = res.x;
-    state.mouse.y = res.y;
-    update();
-    next();
-  })
-);
+var color = randomcolor();
+var cuid = require("cuid");
+var myId = cuid();
 
-const state = {
-  mouse: {
-    x: getRandomInt(0, 1600),
-    y: getRandomInt(0, 800)
-  }
+window.addEventListener("mousemove", onMouseMove);
+
+var state = {
+  squares: [
+    {
+      id: myId,
+      color: color,
+      x: getRandomInt(0, 1600),
+      y: getRandomInt(0, 800)
+    }
+  ]
 };
+
+ws.pipe(websocketHandler());
 
 update();
 
@@ -30,17 +29,32 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function update() {
-  console.log("Je djoum au moin ici ?");
-  return html.update(
-    root,
-    `<div class="square" style="top: ${state.mouse.y}px; left: ${state.mouse
-      .x}px; background:${color}" ></div>`
-  );
+function square(info) {
+  return `<div class="square" style="top: ${info.y}px; left: ${info.x}px; background:${info.color}" ></div>`;
 }
 
-window.addEventListener("mousemove", onMouseMove);
+function update() {
+  return html.update(root, `${state.squares.map(sq => square(sq))}`);
+}
 
 function onMouseMove(e) {
-  ws.write(JSON.stringify({ x: e.clientX, y: e.clientY }));
+  ws.write(JSON.stringify({ x: e.clientX, y: e.clientY, id: myId }));
+}
+
+function websocketHandler() {
+  return through(function(buf, enc, next) {
+    console.log("avant le parsing", buf.toString());
+    var res = JSON.parse(buf.toString());
+    console.log("retour  socket", res);
+    if (state.squares.some(sq => sq.id === res.id)) {
+      state.squares.map(sq => {
+        if (sq.id === res.id) return Object.assign(sq, { x: res.x, y: res.y });
+        return sq;
+      });
+    } else {
+      state.squares.push(res);
+    }
+    update();
+    next();
+  });
 }
